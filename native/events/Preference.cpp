2f3cb495428117335425561f3dac1b69cdd9f4b0
zhangzhao.a <zhangzhao.a@bytedance.com>
2023-08-16 17:25:17 +0800

    fix license

--- native/events/Preference.cpp
+++ native/events/Preference.cpp
@@ -1,20 +1,8 @@
 /*
- * Copyright (c) 2020 Bytedance Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This code is licensed under the Fastbot license. You may obtain a copy of this license in the LICENSE.txt file in the root directory of this source tree.
  */
 /**
- * @authors Jianqiang Guo, Yuhui Su
+ * @authors Jianqiang Guo, Yuhui Su, Zhao Zhang
  */
 #include <fstream>
 #include <sstream>
